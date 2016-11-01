734ea20ba72baf9fc443f8ac7af675ed1075b362
joehua87 <joehua87@gmail.com>
2016-11-01 13:12:36 +0700

    declare external flow modules

---
+++ flow-typed/external-modules.js
@@ -0,0 +1,43 @@
+declare module 'CSSModule' {
+  declare var exports: { [key: string]: string };
+}
+
+declare module 'debug' {
+  declare var exports: any;
+}
+
+declare module 'electron' {
+  declare var exports: any;
+}
+
+declare module 'redux' {
+  declare var exports: any;
+}
+
+declare module 'react-redux' {
+  declare var exports: any;
+}
+
+declare module 'redux-thunk' {
+  declare var exports: any;
+}
+
+declare module 'react-router' {
+  declare var exports: any;
+}
+
+declare module 'react-router-redux' {
+  declare var exports: any;
+}
+
+declare module 'webpack' {
+  declare var exports: any;
+}
+
+declare module 'webpack-dev-middleware' {
+  declare var exports: any;
+}
+
+declare module 'webpack-hot-middleware' {
+  declare var exports: any;
+}
