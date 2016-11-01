07b2f389dd7f6860046af2ebeea7a5d2328b15da
joehua87 <joehua87@gmail.com>
2016-11-01 14:34:51 +0700

    refactor resolve

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -1,6 +1,7 @@
 // @flow
+
 import React, { Component } from 'react';
-import Home from '../components/Home';
+import Home from 'components/Home';
 
 export default class HomePage extends Component {
   render() {
--- app/index.js
+++ app/index.js
@@ -4,8 +4,8 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { Router, hashHistory } from 'react-router';
 import { syncHistoryWithStore } from 'react-router-redux';
-import routes from './routes';
-import configureStore from './store/configureStore';
+import routes from 'routes';
+import configureStore from 'store/configureStore';
 import './app.global.css';
 
 const store = configureStore();
